c09a17a2f0d83228933e886912e2305259e1f65f
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2021-03-23 20:31:55 +0600

    first node project

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+
+const fruits = {
+    name: 'banana', 
+    price: 400
+}
 
 app.get('/', (req, res) => {
-    res.send('Thank you for calling me');
+    res.send('thank you for your response nodemon');
 });
 
-app.listen(3000, () => console.log('Listening to port 3000'));
+app.get('/fruits/banana', (req, res) => {
+    res.send(fruits)
+});
+const user = ['kamal', 'jamal', 'sabir', 'jabil'];
+
+app.get('/users/:id', (req, res) => {
+    const Id = req.params.id
+    const name = user[Id]
+    // res.send(name)
+    res.send({Id, name})
+})
+
+app.post('/addUser', (req, res) => {
+    const user = req.body;
+    user.id = 55;
+    res.send(req.body)
+})
+
+app.listen(8000, () => console.log('Listening to port 8000'));
